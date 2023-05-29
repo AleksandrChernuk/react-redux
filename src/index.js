352d7c21b4f163989fe42385f4d8dc12ee0f5d85
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2023-05-29 21:11:30 +0300

    s

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { persistor } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-redux">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
